c63d5ceed804056b74907846fe22d9f36a7f6e66
Blake Calhoun <blakecalhoun35@gmail.com>
2017-12-07 21:28:30 -0600

    Add files via upload

---
+++ R Taco Menu.r
@@ -0,0 +1,52 @@
+> taco.picadillo
+[1] 2.75
+> taco.roasted_pork
+[1] 2.75
+> taco.beef_fajita
+[1] 3.25
+> taco.chicken_fajita <- 2.75
+> taco.brisket <- 3.25
+> taco.rajas <- 2.50
+> taco.fish <- 3
+> taco.baja_shrimp <- 3.25
+> taco.rusty <- 2.75
+> taco.fried_chicken <- 2.75
+> taco.bbq_brisket <- 3.25
+> taco.black_bean <- 2.50
+> taco.texican <- 2.75
+> taco.gringo <- 2.75
+> avocado <- .35
+> breakfast.bacon_egg_cheese <- 2.75
+> breakfast.chorizo_egg_cheese <- 2.75
+> breakfast.jalapeno_sausage_egg_cheese <- 2.75
+> breakfast.brisket_egg_cheese <- 2.75
+> breakfast.brisket_egg_cheese <- 3.25
+> breakfast.beef_fajita_egg_cheese <- 3.25
+> breakfast.potato_egg_cheese <- 2.50
+> nachos <- 5
+> topping.black_bean <- .75
+> topping.chicken <- .75
+> topping.queso_cheese <- .75
+> topping.spicy_beef <- .75
+> topping.chorizo <- .75
+> kid.quesadilla <- 2
+> kid.brisket_taco <- 3.25
+> kid.chicken_taco <- 2.75
+> ls()
+ [1] "avocado"                               "breakfast.bacon_egg_cheese"
+ [3] "breakfast.beef_fajita_egg_cheese"      "breakfast.brisket_egg_cheese"
+ [5] "breakfast.chorizo_egg_cheese"          "breakfast.jalapeno_sausage_egg_cheese"
+ [7] "breakfast.potato_egg_cheese"           "kid.brisket_taco"
+ [9] "kid.chicken_taco"                      "kid.quesadilla"
+[11] "nachos"                                "taco.baja_shrimp"
+[13] "taco.bbq_brisket"                      "taco.beef_fajita"
+[15] "taco.black_bean"                       "taco.brisket"
+[17] "taco.chicken_fajita"                   "taco.fish"
+[19] "taco.fried_chicken"                    "taco.gringo"
+[21] "taco.picadillo"                        "taco.rajas"
+[23] "taco.roasted_pork"                     "taco.rusty"
+[25] "taco.texican"                          "topping.black_bean"
+[27] "topping.chicken"                       "topping.chorizo"
+[29] "topping.queso_cheese"                  "topping.spicy_beef"
+> sides.chips_salsa <- 2.50
+> sides.chips_pico_de_gallo <- 2.50
